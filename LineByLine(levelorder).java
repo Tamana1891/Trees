6f2915b75545faf9e966acdbe4455d23838771a7
Tamana <35145055+Tamana1891@users.noreply.github.com>
2019-02-16 10:32:03 +0530

    Add files via upload

---
+++ LineByLine(levelorder).java
@@ -0,0 +1,23 @@
+class Main
+{
+    void levelOrder_LineByLine(Node node) 
+    {
+        Queue<Node> q = new LinkedList<Node>();
+        q.add(node);
+        while(true){
+            int count = q.size();
+            if(count == 0)
+                break;
+            while(count > 0){
+                Node temp = q.poll();
+                System.out.print(temp.data+" ");
+                if(temp.left != null)
+                    q.add(temp.left);
+                if(temp.right != null)
+                    q.add(temp.right);
+                    count--;
+            }
+            System.out.print("$"+" ");
+        }
+    }
+}
